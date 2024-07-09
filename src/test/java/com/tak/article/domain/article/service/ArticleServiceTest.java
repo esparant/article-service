d3d57d67cb18c38d38e2136b458e84810afc06db
esparant <esparant2@gmail.com>
2024-07-09 21:28:52 +0900

    삭제 기능, 검색 기능 테스트 완료

--- src/test/java/com/tak/article/domain/article/service/ArticleServiceTest.java
+++ src/test/java/com/tak/article/domain/article/service/ArticleServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.tak.article.domain.article.entity.Post;
 import com.tak.article.domain.article.form.PostForm;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,7 +43,29 @@ class ArticleServiceTest {
 
         assertThat(result.getTitle()).isEqualTo("nono");
         assertThat(result.getContent()).isEqualTo("oh my god");
-
     }
 
+    @Test
+    @DisplayName("게시글 삭제")
+    void deletePost() {
+        Post post = new Post("hello", "hello", "me");
+        articleService.writePost(post);
+        articleService.deletePost(post.getId());
+        assertThat(articleService.getPostList()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("검색 시스템")
+    void searchPost() {
+        articleService.writePost(new Post("hello", "hello", "me"));
+        articleService.writePost(new Post("hello2", "hello2", "me2"));
+        articleService.writePost(new Post("hello3", "hello3", "me3"));
+
+        List<Post> resultA = articleService.searchPost("hello");
+        assertThat(resultA).hasSize(3);
+
+        List<Post> resultB = articleService.searchPost("hello2");
+        assertThat(resultB).hasSize(1);
+
+    }
 }
